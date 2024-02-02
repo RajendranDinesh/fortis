42b2e35da0e923a2c5760ebeca402ffee24c60bc
Adesh <adeshdance@gmail.com>
2024-02-02 11:22:14 +0530

    Added Classroom page skeleton and CSV parse in add students Modal.

--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StaffDashboard from "./dashboard";
+import Classroom from "./Classroom";
 
 function Staff() {
     return (
         <Routes>
             <Route path="dashboard" element={<StaffDashboard />} />
+            <Route path="classroom" element={<Classroom />} />
         </Routes>
     );
 };
